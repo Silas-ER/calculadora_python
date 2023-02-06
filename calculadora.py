9fb0282e2113fbcfe6fd00ac792bcd5df2803d18
Silas Eduardo <90927889+SilasEd@users.noreply.github.com>
2023-02-06 13:58:06 -0300

    definicao de comandos

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,31 @@
+def print_boasvindas():
+    print("+-*/+-*/+-*/+-*/+-*/+-*/+-*/+-*/+-*/+-*/")
+    print("+-*/+-*/+-Calduladora Python+-*/+-*/+-*/")
+    print("+-*/+-*/+-*/+-*/+-*/+-*/+-*/+-*/+-*/+-*/")
 
+def soma(n1, n2):
+    return n1 + n2
+
+def subtracao(n1, n2):
+    return n1 - n2 
+
+def multiplicacao(n1, n2):
+    return n1*n2
+
+def divisao(n1, n2):
+    return n1/n2
+
+def tabuada_geral():
+    n = range(1,11)
+    n1 = range(1,11)
+    tabuada = [n * n1 for ]
+
+def tabuada_uni(n):
+    n2 = range(1,11)
+    tabuada = [n2*n in n2]
+    print(tabuada)
+    
+def opcoes():
+    escolha = input("Qual operação deseja fazer? ")
+    if(escolha == "somar"):
+        soma()
